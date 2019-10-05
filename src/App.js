41a1dcbfa65c9aa351fc01da655fbe875ec973b6
Fahofah <fahriulucaycy@gmail.com>
2019-10-05 18:12:57 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          docker-compose is cool
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to Learn React
         </a>
       </header>
     </div>
