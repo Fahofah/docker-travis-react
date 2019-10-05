7ac242b4d3f0199715ddcb0baa9056de4599c7d1
Fahofah <fahriulucaycy@gmail.com>
2019-10-05 20:30:06 +0100

    changed message

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          docker-compose is cool
+          I was changed in the feature branch
         </p>
         <a
           className="App-link"
